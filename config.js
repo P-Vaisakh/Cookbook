a593fe42c8914360f5b48eb35c51c9387c1d9701
P-Vaisakh <pvaisakh2002@gmail.com>
2023-10-02 12:11:39 +0530

    final commit

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
-// let apiKey1="bbdcbdb0ce904d12a834dd09e7794c24"
-// let apiKey2="0916fad6f8634e2ba16f4cc77ab02b12"
-// let apiKey3 = "4d51e0af4af1488cb899ce4c2c22dcb8";
-// let apiKey4="3f465599cd3f40bb8037b3dde60cea75"
-
 // const apiURL = "https://api.spoonacular.com/recipes/random";
 
 export const apiKey = "bbdcbdb0ce904d12a834dd09e7794c24";
